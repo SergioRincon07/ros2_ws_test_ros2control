54a7ce9679d11ee215729d51a011c0a78e99a9b0
Sergio David Rincon Murcia <sergiorincon50@gmail.com>
2024-02-19 20:38:10 -0500

    se cambia el test de posicion por el de velocidad

--- src/test2_ros2control/bringup/launch/rrbot.launch.py
+++ src/test2_ros2control/bringup/launch/rrbot.launch.py
@@ -104,7 +104,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+        arguments=["forward_velocity_controller", "--controller-manager", "/controller_manager"],
     )
 
     # Delay rviz start after `joint_state_broadcaster`
